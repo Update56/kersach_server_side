f3caf2b3fbdc2b0ea54c4265d9aeeb9a49f4153f
Update56 <heupdate56@gmail.com>
2024-06-11 19:52:22 +0700

    Добавлена тестовая отправка списка пользователей клинту

--- server.go
+++ server.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"bufio"
+	"encoding/gob"
 	"fmt"
 	"net"
+	"strings"
+	"time"
 )
 
+// структура сообщения
+type Message struct {
+	Sender   string
+	Receiver string
+	Text     string
+}
+
+// зарезервированное спец.имя клиента
+const clientName string = "client"
+
+// зарезервированное спец.имя сервера
+const serverName string = "server"
+
 func main() {
-	fmt.Println("Hello Server!")
+	fmt.Println("Hello, I'am Server!")
 	//открытие порта сервера по протоколу tcp
 	listener, err := net.Listen("tcp", ":56565")
 	//првоерка на ошибку при открытии
@@ -15,33 +30,55 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	//закрытие в конце выполнения программы
+	//закрытие порта в конце выполнения программы
 	defer listener.Close()
 	fmt.Println("Server is listening...")
 
-	for {
-		//создание потока при подтверждении подключении
+	//создание хэш-таблицы ключ - никнейм, значение - подключение
+	connMap := make(map[string]net.Conn)
+
+	for i := 0; ; i++ {
+		//подтверждение подключения
 		conn, err := listener.Accept()
 		//проверка на ошибку
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		connProc(conn)
+		connMap[string(i)] = conn
+		//вызываем функцию в отдельной горутине
+		go connProc(conn, connMap)
+
 	}
 }
 
-func connProc(conn net.Conn) {
+func connProc(conn net.Conn, connMap map[string]net.Conn) {
 	defer conn.Close()
+	enc := gob.NewEncoder(conn)
+	enc.Encode(formSpecialMessages("001", keysToString(connMap)))
 	for {
-		line := make([]byte, 1024)
-		buf := bufio.NewReader(conn)
-		n, err := buf.Read(line)
-		//проверка на считывание и ошибку
-		if n == 0 || err != nil {
-			fmt.Println("Read error:", err)
-			break
-		}
-		conn.Write([]byte("Hello " + string(line[:n]) + "! I'am Server"))
+		time.Sleep(time.Second * 5)
 	}
+}
+
+// функция копирования ключей хэш-мапы в строку
+func keysToString(connMap map[string]net.Conn) string {
+
+	//cоздание слайса для хранения ключей
+	keys := make([]string, 0, len(connMap))
+
+	//заполнение слайса ключами мапы
+	for key := range connMap {
+		keys = append(keys, key)
+	}
+	//объединение ключей в одну строку
+	result := strings.Join(keys, "\n")
+	//возвращаем полученную строку
+	return result
+}
+
+// функция формирования специального сообщения
+func formSpecialMessages(code string, text string) Message {
+	messText := (code + "\n" + text)
+	return Message{serverName, clientName, messText}
 }
