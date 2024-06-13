f8a027f8c7a01e06f7dbe1640d8922129230f178
Update56 <heupdate56@gmail.com>
2024-06-13 22:48:11 +0700

    Добавлена обратока ошибки при разрыве соединения

--- server.go
+++ server.go
@@ -73,49 +73,69 @@ func main() {
 }
 
 func connProc(conn net.Conn, connMap map[string]net.Conn, infoLog *log.Logger) {
-	//3.Отправка изменённого списка
+	// 3. Отправка изменённого списка
 	defer sendCurrentList(connMap)
-	//2.Закрытие соединения
+	// 2. Закрытие соединения
 	defer conn.Close()
-	//объявляем входной декодер с потоком в виде подключения
+	// объявляем входной декодер с потоком в виде подключения
 	dec := gob.NewDecoder(conn)
-	//пустая структура для сообщения
+	// пустая структура для сообщения
 	recMess := Message{}
-	//десериализация полученного "нулевого" сообщения
-	dec.Decode(&recMess)
-	//объявляем новую запись в хэш-мапе
+	// десериализация полученного "нулевого" сообщения
+	if err := dec.Decode(&recMess); err != nil {
+		infoLog.Println("Ошибка декодирования сообщения:", err)
+		return
+	}
+	//ник подключившегося юзера
+	nickname := recMess.Sender
+	// объявляем новую запись в хэш-мапе
 	connMap[recMess.Sender] = conn
-	//лог в консоль
+	// лог в консоль
 	fmt.Println("User:", recMess.Sender, "connected")
-	//лог в файл
+	// лог в файл
 	infoLog.Println("User:", recMess.Sender, "connected")
 
 	sendCurrentList(connMap)
-	//1.Удаление записи
+	// 1. Удаление записи
 	defer delete(connMap, recMess.Sender)
 
 	for {
-		//объявляем входной декодер с потоком
+		// Установка тайм-аута на чтение
+		conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+
+		// объявляем входной декодер с потоком
 		dec := gob.NewDecoder(conn)
-		//пустая структура для сообщения
+		// пустая структура для сообщения
 		recMess := Message{}
-		//десериализация полученного сообщения
-		dec.Decode(&recMess)
-		//обработка отключения от сервера
+		// десериализация полученного сообщения и проверка на ошибки
+		if err := dec.Decode(&recMess); err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				// Таймаут произошел, но соединение может быть еще открытым
+				continue
+			}
+			infoLog.Println("User:", nickname, "disconnected")
+			fmt.Println("User:", nickname, "disconnected")
+			break
+		}
+		// Обработка отключения от сервера
 		if recMess.Receiver == serverName && len(recMess.Text) >= len("Disconnect") {
 			if recMess.Text[:len("Disconnect")] == "Disconnect" {
-				//логирование
-				infoLog.Println("User:", recMess.Sender, "disconnected")
-				fmt.Println("User:", recMess.Sender, "disconnected")
+				// Логирование
+				infoLog.Println("User:", nickname, "disconnected")
+				fmt.Println("User:", nickname, "disconnected")
 				return
 			}
 		}
-		//создаём выходной енкодер с соединение соотвествующим отправителю
-		enc := gob.NewEncoder(connMap[recMess.Receiver])
-		//лог сообщения
-		infoLog.Println(recMess.Receiver, "to", recMess.Receiver, ": ", recMess.Text)
-		//передача сообщения
-		enc.Encode(recMess)
+		// Создаём выходной энкодер с соединением, соответствующим отправителю
+		encoder := gob.NewEncoder(connMap[recMess.Receiver])
+		// Лог сообщения
+		infoLog.Println(recMess.Sender, "to", recMess.Receiver, ": ", recMess.Text)
+		// Передача сообщения
+		if err := encoder.Encode(recMess); err != nil {
+			infoLog.Println("Ошибка кодирования сообщения:", err)
+			fmt.Println("Ошибка кодирования сообщения:", err)
+			break
+		}
 	}
 }
 
